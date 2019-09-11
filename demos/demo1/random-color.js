ee3f587896d6299c1754ec9bd939cc8fccfbd847
Sonny Najar <sonnynajar@gmail.com>
2019-09-11 13:26:46 -0700

    changed to work with vr-engine

--- demos/demo1/random-color.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-((AFRAME["registerComponent"])("random-color",{schema:{},init:(function(){var randomHue=((Math.floor)((((Math.random)())*360)));return ((this.el["setAttribute"])("color",("hsl("+randomHue+",100%,50%)")));})}));
