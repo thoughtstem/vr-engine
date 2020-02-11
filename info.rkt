363c8e2d3abc364bd63e2d1c2af8a0b972c83bb4
sonnynajar <sonny.najar@gmail.com>
2020-02-11 14:40:16 -0800

    made contract docs

--- info.rkt
+++ info.rkt
@@ -7,6 +7,7 @@
                "urlang"
                "https://github.com/thoughtstem/define-assets-from.git"
                "https://github.com/thoughtstem/image-coloring.git"
+               "https://github.com/thoughtstem/TS-GE-Katas.git?path=ts-kata-util"
                ))
 
 (define scribblings '(("scribblings/manual.scrbl" (multi-page))))
